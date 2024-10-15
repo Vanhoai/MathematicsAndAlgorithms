7bfcdf8ebae02ee54d61c63ffd17f6c487f9e8e1
Aurora Study <hardy251223@gmail.com>
2024-10-15 21:34:39 +0700

    feat: impl dynamic array

--- data_struture/array.cpp
+++ data_struture/array.cpp
@@ -3,23 +3,93 @@
 //
 
 #include "array.h"
-using namespace std;
+#include <iostream>
+#include <stdexcept>
 
-template <class T>
-class Array {
+template<typename T>
+class Vector {
+private:
+    T* arr;
+    size_t capacity;
+    size_t length;
+
 public:
-    static constexpr int MAX_SIZE = 1001;
-    Array() {
-        this->last = -1;
-        this->current = -1;
+    Vector() {
+        capacity = 1;
+        length = 0;
+        arr = new T[capacity];
     }
 
-    bool isEmpty() {
-        return this->last < 0;
+    ~Vector() {
+        delete[] arr;
+    }
+
+    [[nodiscard]] size_t size() const {
+        return length;
+    }
+
+    [[nodiscard]] bool empty() const {
+        return length == 0;
+    }
+
+    T& operator[](size_t index) {
+        if (index >= length) {
+            throw std::out_of_range("Index out of range");
+        }
+        return arr[index];
+    }
+
+    const T& operator[](size_t index) const {
+        if (index >= length) {
+            throw std::out_of_range("Index out of range");
+        }
+        return arr[index];
+    }
+
+    void push_back(const T& value) {
+        if (length == capacity) {
+        }
+        arr[length++] = value;
+    }
+
+    void pop_back() {
+        if (length > 0) {
+            --length;
+        }
+    }
+
+    void insert(const T& value, size_t index) {
+        if (index > length) {
+            throw std::out_of_range("Index out of range");
+        }
+        if (length == capacity) {
+            resize();
+        }
+        for (size_t i = length; i > index; --i) {
+            arr[i] = arr[i - 1];
+        }
+        arr[index] = value;
+        ++length;
+    }
+
+    void erase(size_t index) {
+        if (index >= length) {
+            throw std::out_of_range("Index out of range");
+        }
+        for (size_t i = index; i < length - 1; ++i) {
+            arr[i] = arr[i + 1];
+        }
+        --length;
     }
 
 private:
-    T data[MAX_SIZE];
-    int last = -1;
-    int current = -1;
+    void resize() {
+        capacity *= 2;
+        T* newArr = new T[capacity];
+        for (size_t i = 0; i < length; ++i) {
+            newArr[i] = arr[i];
+        }
+        delete[] arr;
+        arr = newArr;
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "data_struture/array.cpp"
 using namespace std;
 
 int main() {
+    Vector<int> vec;
+
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+    vec.push_back(40);
+    vec.push_back(50);
+
+    vec.insert(60, 2);
+
+    for (size_t i = 0; i < vec.size(); ++i) {
+        std::cout << vec[i] << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
