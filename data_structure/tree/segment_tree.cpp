0e18497b7de499a47ffb720cf72ea19e9a66edc2
Aurora Study <hardy251223@gmail.com>
2024-10-19 23:28:27 +0700

    feat: segment tree

--- data_structure/tree/segment_tree.cpp
+++ data_structure/tree/segment_tree.cpp
@@ -1,42 +1,73 @@
+#include <cmath>
 #include <iostream>
 #include <vector>
 using namespace std;
 
-class SegmentTree {
-private:
-    vector<int> tree;
-    int size;
+#define MAX 1001
 
-    void buildTree(vector<int> &arr, const int node, const int start, const int end) {
-        if (start == end) {
-            tree[node] = arr[start];
+int a[MAX], segTree[4 * MAX];
+
+void buildTree(int node, int start, int end) {
+    if (start == end) {
+        segTree[node] = a[start];
+    } else {
+        int mid = (start + end) / 2;
+        buildTree(2 * node, start, mid);
+        buildTree(2 * node + 1, mid + 1, end);
+
+        segTree[node] = segTree[2 * node] + segTree[2 * node + 1];
+    }
+}
+
+void update(int node, int start, int end, int index, int value) {
+    if (start == end) {
+        segTree[node] = value;
+    } else {
+        const int mid = (start + end) / 2;
+        if (index <= mid) {
+            update(2 * node, start, mid, index, value);
         } else {
-            const int mid = (start + end) / 2;
-            buildTree(arr, 2 * node, start, mid);
-            buildTree(arr, 2 * node + 1, mid + 1, end);
-
-            tree[node] = tree[2 * node] + tree[2 * node + 1];
+            update(2 * node + 1, mid + 1, end, index, value);
         }
+
+        segTree[node] = segTree[2 * node] + segTree[2 * node + 1];
+    }
+}
+
+int query(int node, int start, int end, int left, int right) {
+    if (left > right)
+        return 0;
+
+    if (start == left && end == right)
+        return segTree[node];
+    else {
+        const int mid = (start + end) / 2;
+        int s1 = query(2 * node, start, mid, left, min(mid, right));
+        int s2 = query(2 * node + 1, mid + 1, end, max(mid + 1, left), right);
+        return s1 + s2;
+    }
+}
+
+// 8
+// 1 2 3 4 5 6 7 8
+
+// input: n = 8
+// a = { 1, 2, 3, 4, 5, 6, 7, 8 }
+// => segment tree: 36 10 26 3 7 11 15 1 2 3 4 5 6 7 8
+// a = { 1, 2, 3, 4, 8, 6, 7, 8 }
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
 
-public:
-    SegmentTree(vector<int> &arr) {
-        size = arr.size();
-        tree.resize(4 * size);   // Size tree will be 4*n
-        buildTree(arr, 1, 0, size - 1);
-    }
+    buildTree(1, 0, n - 1);
+    update(1, 0, n - 1, 4, 8);
 
-    void printTree() {
-        for (int i = 1; i < 2 * size; i++) {
-            cout << tree[i] << " ";
-        }
-        cout << endl;
-    }
-};
+    int sum = query(1, 0, n - 1, 5, 7);
+    cout << sum << endl;
 
-// int main() {
-//     vector<int> arr = {1, 3, 5, 7, 9, 11};
-//     SegmentTree st(arr);
-//     st.printTree();
-//     return 0;
-// }
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,36 +1,4 @@
-#include "data_structure/tree/segment_tree.cpp"
 #include <iostream>
-#include <vector>
 using namespace std;
 
-#define max 1001
-
-int a[max], segTree[4 * max];
-
-void buildTree(int node, int start, int end) {
-    if (start == end) {
-        segTree[node] = a[start];
-    } else {
-        int mid = (start + end) / 2;
-        buildTree(2 * node, start, mid);
-        buildTree(2 * node + 1, mid + 1, end);
-
-        segTree[node] = segTree[2 * node] + segTree[2 * node + 1];
-    }
-}
-
-int main() {
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-
-    buildTree(1, 0, n - 1);
-
-    for (int i = 1; i < 2 * n; i++) {
-        cout << segTree[i] << " ";
-    }
-
-    return 0;
-}
+int main() { P }
