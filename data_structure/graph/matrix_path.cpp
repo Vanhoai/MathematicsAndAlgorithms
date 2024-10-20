a417f97e67cc6a7374a451648ca57da37d6db2ee
Aurora Study <hardy251223@gmail.com>
2024-10-20 19:16:13 +0700

    feat: maxtrix path

--- data_structure/graph/matrix_path.cpp
+++ data_structure/graph/matrix_path.cpp
@@ -2,51 +2,61 @@
 #include <queue>
 using namespace std;
 
-// 5 5
-// 1 0 1 0 0
-// 1 0 1 1 1
-// 1 1 1 1 1
-// 1 0 0 1 0
-// 0 0 0 1 0
+// 4 4 4 3
+// o o o x
+// x o x o
+// o o x x
+// o o A x
 
-int n, m;
-int a[1001][1001];
+int n, m, x, y;
+char a[1001][1001];
+int step[1001][1001];
 int dx[4] = {-1, 0, 0, 1};
 int dy[4] = {0, -1, 1, 0};
 
 void input() {
-    cin >> n >> m;
+    cin >> n >> m >> x >> y;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
             cin >> a[i][j];
         }
     }
+
+    memset(step, 0, sizeof(step));
 }
 
-void dfs(int i, int j) {
-    a[i][j] = 0;
+bool dfs(int i, int j) {
+    if (a[i][j] == 'A') return true;
+    a[i][j] = 'x';
     for (int k = 0; k < 4; k++) {
         int x = i + dx[k];
         int y = j + dy[k];
-        if (a[x][y] == 1 && x >= 1 && x <= n && y >= 1 && y <= m) {
-            dfs(x, y);
+        if (a[x][y] != 'x' && x >= 1 && x <= n && y >= 1 && y <= m) {
+            if (dfs(x, y)) return true;
         }
     }
+
+    return false;
 }
 
 void bfs(int i, int j) {
     queue<pair<int, int> > q;
     q.push(make_pair(i, j));
-    a[i][j] = 0;
+    a[i][j] = 'x';
+    step[i][j] = 0;
     while (!q.empty()) {
         pair<int, int> front = q.front();
+        int i = front.first;
+        int j = front.second;
         q.pop();
         for (int k = 0; k < 4; k++) {
-            int x = front.first + dx[k];
-            int y = front.second + dy[k];
-            if (a[x][y] == 1 && x >= 1 && x <= n && y >= 1 && y <= m) {
-                a[x][y] = 0;
-                q.push(make_pair(x, y));
+            int i1 = i + dx[k];
+            int j1 = j + dy[k];
+            if (a[i1][j1] != 'x' && i1 >= 1 && i1 <= n && j1 >= 1 && j1 <= m) {
+                step[i1][j1] = step[i][j] + 1;
+                if (a[i1][j1] == 'A') return;
+                a[i1][j1] = 'x';
+                q.push(make_pair(i1, j1));
             }
         }
     }
@@ -67,6 +77,13 @@ int countConnected() {
 
 int main() {
     input();
-    cout << "Connected components: " << countConnected() << endl;
+    bfs(1, 1);
+
+    if (step[x][y] == 0) {
+        cout << "No path" << endl;
+    } else { 
+        cout << step[x][y] << endl;
+    }
+
     return 0;
 }
