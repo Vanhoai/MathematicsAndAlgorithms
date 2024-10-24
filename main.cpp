ed271efaaed9f1a28162f5fcf874178584a8aedf
Aurora Study <hardy251223@gmail.com>
2024-10-24 14:01:35 +0700

    feat: kruskal algorithms

--- main.cpp
+++ main.cpp
@@ -1,33 +1,88 @@
-#include <cstdio>
-#include <cstdlib>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
+#include <iostream>
+#include <vector>
+using namespace std;
 
-int main(int argc, char *argv[]) {
-    __darwin_pid_t pid;
-    pid = fork();
+struct Edge {
+    int u, v;
+    int w;
+};
 
-    if (pid > 0) {
-        printf("PARENTS | PID = %ld | PPID = %ld\n", (long) getpid(),
-               (long) getppid());
+int n, m;
+int parent[1001];
+int sz[1001];
+vector<Edge> edges;
 
-        if (argc > 2) {
-            printf("PARENTS | There are %d arguments\n", argc - 1);
-            wait(nullptr);
+void makeSet() {
+    for (int i = 1; i <= n; i++) {
+        parent[i] = i;
+        sz[i] = 1;
+    }
+}
+
+int find(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find(parent[v]);
+}
+
+bool Union(int u, int v) {
+    u = find(u);
+    v = find(v);
+
+    if (u == v)
+        return false;
+
+    if (sz[u] < sz[v])
+        swap(u, v);
+
+    parent[v] = u;
+    sz[u] += sz[v];
+    return true;
+}
+
+void input() {
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int x, y, w;
+        cin >> x >> y >> w;
+        Edge e = {x, y, w};
+        edges.push_back(e);
+    }
+}
+
+bool comparator(Edge a, Edge b) { return a.w < b.w; }
+
+void kruskal() {
+    vector<Edge> mst;
+    int cost = 0;
+    sort(edges.begin(), edges.end(), comparator);
+
+    for (Edge e : edges) {
+        if (Union(e.u, e.v)) {
+            mst.push_back(e);
+            cost += e.w;
         }
     }
 
-    if (pid == 0) {
-        printf("CHILDREN | PID = %ld | PPID = %ld\n", (long) getpid(),
-               (long) getppid());
-
-        printf("CHILDREN | List of arguments: \n");
-
-        for (int i = 1; i < argc; i++) {
-            printf("%s\n", argv[i]);
-        }
+    cout << "Cost: " << cost << " Size: " << mst.size() << endl;
+    for (Edge e : mst) {
+        cout << e.u << " " << e.v << endl;
     }
+}
 
-    exit(0);
+// 6 8
+// 1 2 1
+// 3 7 1
+// 6 7 1
+// 2 3 2
+// 5 6 2
+// 4 5 3
+// 1 3 4
+// 3 4 10
+
+int main() {
+    input();
+    makeSet();
+    kruskal();
+    return 0;
 }
