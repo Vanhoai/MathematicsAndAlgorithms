9dbeafc90b72511cc7b010b4824b623bb499dbae
Aurora Study <hardy251223@gmail.com>
2024-10-25 16:00:49 +0700

    feat: update algorithms

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -6,30 +7,37 @@
 #include <vector>
 using namespace std;
 
-vector<pair<int, int>> pi;   // edges
-int a[1001][1001];           // matrix
-vector<int> adj[1001];
+// input: {1, 2, 4, 5, 7, 6} n = 6
+// output is missing number is 3
 
-// 1: { 2, 3 }
-// 2: { 3, 4 }
+// 2
+// 6
+// 1 2 4 5 7 6
+// 5
+// 1 2 3 4 5
+
+void solve() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        int expected_sum = (n + 1) * (n + 2) / 2;
+        int actual_sum = 0;
+        for (int i = 1; i <= n; i++) {
+            int x;
+            cin >> x;
+            actual_sum += x;
+        }
+
+        int ans = expected_sum - actual_sum;
+        cout << ans << endl;
+    }
+}
 
 int main() {
-
-    int pid;
-    pid = fork();
-
-    if (pid > 0) {
-        printf("I am the parent process\n");
-        wait(nullptr);
-        exit(0);
-    } else if (pid == 0) {
-        execl("/bin/ls", "ls", "-l", nullptr);
-        printf("I am the child process\n");
-        exit(0);
-    } else {
-        printf("fork failed\n");
-        exit(-1);
-    }
-
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    solve();
     return 0;
 }
