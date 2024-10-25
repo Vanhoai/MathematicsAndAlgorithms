03f5bd7f58aeae9d2c2da0fd1367f0c79a9ff861
Aurora Study <hardy251223@gmail.com>
2024-10-25 21:35:25 +0700

    feat: set up in sublime text

--- main.cpp
+++ main.cpp
@@ -1,43 +1,24 @@
 #include <iostream>
 #include <map>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
 #include <vector>
 using namespace std;
 
-// input: {1, 2, 4, 5, 7, 6} n = 6
-// output is missing number is 3
-
-// 2
-// 6
-// 1 2 4 5 7 6
-// 5
-// 1 2 3 4 5
-
-void solve() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int n;
-        cin >> n;
-        int expected_sum = (n + 1) * (n + 2) / 2;
-        int actual_sum = 0;
-        for (int i = 1; i <= n; i++) {
-            int x;
-            cin >> x;
-            actual_sum += x;
-        }
-
-        int ans = expected_sum - actual_sum;
-        cout << ans << endl;
-    }
-}
-
 int main() {
+    ios::sync_with_stdio(false);
     cin.tie(0);
-    ios_base::sync_with_stdio(0);
-    solve();
+    #ifndef ONLINE_JUDGE
+        freopen("./in.txt", "r", stdin);
+        freopen("./out.txt", "w", stdout);
+    #endif
+
+    int n;
+    cin >> n;
+    int a[n];
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        sum += a[i];
+    }
+    
     return 0;
 }
