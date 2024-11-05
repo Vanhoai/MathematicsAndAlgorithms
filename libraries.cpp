5d548e10f77ee37c091c3ca337cbab32bd65e926
Aurora Study <hardy251223@gmail.com>
2024-11-05 11:41:46 +0700

    feat: change macros

--- libraries.cpp
+++ libraries.cpp
@@ -4,7 +4,6 @@
 #include <cmath>
 #include <cstdlib>
 #include <cstring>
-#include <string>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -12,6 +11,7 @@
 #include <queue>
 #include <sstream>
 #include <stack>
+#include <string>
 #include <vector>
 using namespace std;
 
@@ -22,7 +22,7 @@ typedef vector<int> vi;      // Alias for vector of integers
 typedef vector<ll> vll;      // Alias for vector of long long
 typedef vector<pi> vii;      // Alias for vector of pairs
 typedef vector<vi> vvi;      // Alias for vector of vectors
-typedef vector<vector<double> > vvd;
+typedef vector<vector<double>> vvd;
 
 // Macros for common operations
 #define F  first       // Short for accessing first element of pair
@@ -40,22 +40,17 @@ typedef vector<vector<double> > vvd;
 #define MEMSET(a, b) memset(a, b, sizeof(a))
 
 // Constants
-#define MOD  1000000007     // A large prime, used as modulo for calculations
-#define INF  1e9            // Used as a large value (infinity) for graphs
-#define EPS  1e-9           // Used as a small value for floating-point comparisons
-#define MAXN 1001           // Common size limit for arrays
+#define MOD  1000000007   // A large prime, used as modulo for calculations
+#define INF  1e9          // Used as a large value (infinity) for graphs
+#define EPS  1e-9   // Used as a small value for floating-point comparisons
+#define MAXN 1001   // Common size limit for arrays
 
 // Utility Macros
-#define GCD(a, b) gcd(a, b)                 // Finds GCD of a and b
-#define LCM(a, b) ((a) * (b) / GCD(a, b))   // Finds LCM of a and b
-#define SQ(x) ((x) * (x))                   // Calculates square of x
-#define SZ(x) ((int) (x).size())            // Returns size of container x
-#define SORT(v) sort(ALL(v))                // Sorts a container in ascending order
-#define MIN(a, l, r) min(a,l,r)             // Min of array       
-#define MAX(a, l, r) max(a,l,r)             // Max of array
-#define SORT_ASC(a, l, r) sort_asc(a, l, r) // Sort array in ascending order
-#define SORT_DESC(a, l, r) sort_desc(a, l, r) // Sort array in descending order
-#define IS_PRIME(x) isPrime(x)             // Checks if x is prime
+#define GCD(a, b)   gcd(a, b)                 // Finds GCD of a and b
+#define LCM(a, b)   ((a) * (b) / GCD(a, b))   // Finds LCM of a and b
+#define SQ(x)       ((x) * (x))               // Calculates square of x
+#define SZ(x)       ((int) (x).size())        // Returns size of container x
+#define IS_PRIME(x) isPrime(x)                // Checks if x is prime
 
 // Fast I/O
 #define FAST_IO                                                                \
@@ -64,7 +59,6 @@ typedef vector<vector<double> > vvd;
     cout.tie(nullptr)
 
 #define READ_WRITE_FILE read
-
 
 // Shortening Cin and Cout (optional for reducing typing)
 #define sc(x) cin >> x            // Shorter input command
@@ -78,74 +72,8 @@ int gcd(int a, int b) {
 }
 
 void read() {
-    #ifndef ONLINE_JUDGE
-        freopen("in.txt", "r", stdin);
-        freopen("out.txt", "w", stdout);
-    #endif
-}
-
-int min(int *a, int l, int r) {
-    if (l == r) return a[l];
-    if (l + 1 == r) {
-        return a[l] > a[r] ? a[r] : a[l];
-    }
-
-    int m = (l + r) >> 1;
-    int m1 = min(a, l, m);
-    int m2 = min(a, m + 1, r);
-
-    return m1 > m2 ? m2 : m1;
-}
-
-int max(int * a, int l, int r) {
-    if (l == r) return a[l];
-    if (l + 1 == r) {
-        return a[l] < a[r] ? a[r] : a[l];
-    }
-
-    int m = (l + r) >> 1;
-    int m1 = max(a, l, m);
-    int m2 = max(a, m + 1, r);
-
-    return m1 < m2 ? m2 : m1;
-}
-
-int isPrime(int n) {
-    if (n < 2) return 0;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) return 0;
-    }
-    return 1;
-}
-
-void sort_asc(int *a, int l, int r) {
-    if (l >= r) return;
-    int i = l, j = r, x = a[(l + r) >> 1];
-    while (i <= j) {
-        while (a[i] < x) i++;   
-        while (a[j] > x) j--;
-        if (i <= j) {
-            swap(a[i], a[j]);
-            i++, j--;
-        }
-    }
-
-    sort_asc(a, l, j);
-    sort_asc(a, i, r);
-}
-
-void sort_desc(int *a, int l, int r) {
-    if (l >= r) return;   
-    int i = l, j = r, x = a[(l + r) >> 1];
-    while (i <= j) {
-        while (a[i] > x) i++;   
-        while (a[j] < x) j--;
-        if (i <= j) {
-            swap(a[i], a[j]);
-            i++, j--;
-        }
-    }
-
-    sort_desc(a, l, j);
-    sort_desc(a, i, r);
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
 }
--- main.cpp
+++ main.cpp
@@ -1,69 +1,9 @@
-
 #include "libraries.cpp"
 using namespace std;
-
-class SegmentTree {
-private:
-    vi st, A;
-    int n;
-    int left(int p) { return p << 1; }
-    int right(int p) { return (p << 1) + 1; }
-
-    void build(int p, int L, int R) {
-        if (L == R)
-            st[p] = A[L];
-        else {
-            build(left(p), L, (L + R) / 2);
-            build(right(p), ((L + R) / 2) + 1, R);
-            int p1 = st[left(p)], p2 = st[right(p)];
-            st[p] = (A[p1] <= A[p2]) ? p1 : p2;
-        }
-    }
-
-public:
-    SegmentTree(const vi &_A) {
-        A = _A;
-        n = (int) A.size();    // copy content for local usage
-        st.assign(4 * n, 0);   // create large enough vector of zeroes
-        build(1, 0, n - 1);    // recursive build
-    }
-
-    int rmq(int p, int L, int R, int i, int j) {   // O(log n)
-        if (i > R || j < L)
-            return -1;   // current segment outside query range
-        if (L >= i && R <= j)
-            return st[p];   // inside query range
-
-        int p1 = rmq(left(p), L, (L + R) / 2, i, j);
-        int p2 = rmq(right(p), (L + R) / 2 + 1, R, i, j);
-
-        if (p1 == -1)
-            return p2;   // if we try to access segment outside query
-        if (p2 == -1)
-            return p1;   // same as above
-
-        return (A[p1] <= A[p2]) ? p1 : p2;   // as in build routine
-    }
-};
 
 int main() {
     FAST_IO;
     READ_WRITE_FILE();
-
-    int n;
-    cin >> n;
-    vi a(n);
-    REP(i, n) cin >> a[i];
-
-    SegmentTree st(a);
-
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int l, r;
-        cin >> l >> r;
-        cout << st.rmq(1, 0, n - 1, l - 1, r - 1) << endl;
-    }
 
     return 0;
 }
