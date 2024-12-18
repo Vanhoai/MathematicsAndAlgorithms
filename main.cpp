b6a2f6fc05893143a2687e38b174638cd2a3e66a
Aurora Study <hardy251223@gmail.com>
2024-12-18 23:18:24 +0700

    feat: set up run cpp in vscode

--- main.cpp
+++ main.cpp
@@ -1,53 +1,21 @@
-#include <cstdlib>
-#include <ctime>
 #include <iostream>
-#include <mutex>
-#include <thread>
 #include <vector>
-
 using namespace std;
 
-#define FAST_IO                \
-  ios::sync_with_stdio(false); \
-  cin.tie(nullptr);            \
-  cout.tie(nullptr)
-#define READ_WRITE_FILE          \
-  freopen("in.txt", "r", stdin); \
-  freopen("out.txt", "w", stdout)
+#define IO                                                                     \
+    ios::sync_with_stdio(false);                                               \
+    cin.tie(0);                                                                \
+    cout.tie(0);
 
-int solve(const string &s) {
-  const int n = s.length();
-  vector<vector<int>> dp(n, vector<int>(n, 0));
-
-  for (int i = 0; i < n; ++i)
-    dp[i][i] = 1;
-
-  for (int len = 2; len <= n; ++len) {
-    for (int i = 0; i < n - len + 1; ++i) {
-      int j = i + len - 1;
-      if (s[i] == s[j]) {
-        dp[i][j] = dp[i + 1][j - 1] + 2;
-      } else {
-        dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
-      }
-    }
-  }
-
-  return dp[0][n - 1];
-}
+#define RWF                                                                    \
+    freopen("in.txt", "r", stdin);                                             \
+    freopen("out.txt", "w", stdout);
 
 int main() {
-  FAST_IO;
-  READ_WRITE_FILE;
+    IO;
+    RWF;
 
-  int TC;
-  cin >> TC;
-  cin.ignore();
-  while (TC--) {
-    string s;
-    getline(cin, s);
-    cout << solve(s) << endl;
-  }
+    cout << "Hello World!" << endl;
 
-  return 0;
+    return 0;
 }
