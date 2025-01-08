ad4333b7607c308d6305e07ee12cb6a45a5d0054
Aurora Study <hardy251223@gmail.com>
2025-01-08 12:44:30 +0700

    feat: update in out file

--- main.cpp
+++ main.cpp
@@ -2,17 +2,11 @@
 #include <iostream>
 using namespace std;
 
+#define IOS ios::sync_with_stdio(false), cin.tie(0)
+#define IO  freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout)
+
 int main() {
-    Matrix A({{-7, -5}, {4, 3}});
-    Matrix B({{-2, -9}, {5, 23}});
-    Matrix C({{2, -5}, {-1, 3}});
-
-    try {
-        Matrix Ans = A.inverse() * C * B.inverse();
-        Ans.print();
-    } catch (const exception &e) {
-        cerr << "Error: " << e.what() << endl;
-    }
-
+    IOS;
+    IO;
     return 0;
 }
