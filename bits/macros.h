d27a3a402f2e460dcff474bc983540f8102e3921
Aurora Study <hardy251223@gmail.com>
2025-02-17 22:24:10 +0700

    feat: set up and add file macros.h to project

---
+++ bits/macros.h
@@ -0,0 +1,45 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define ms(s, n)         memset(s, n, sizeof(s))
+#define all(a)           a.begin(), a.end()
+#define present(t, x)    (t.find(x) != t.end())
+#define sz(a)            int((a).size())
+#define FOR(i, a, b)     for (int i = (a); i < (b); ++i)
+#define INVERSE(i, a, b) for (int i = (a); i >= b; --i)
+#define REP(i, a, b)     for (int i = (a); i <= (b); ++i)
+
+#define PB push_back
+#define PF push_front
+#define MP make_pair
+#define F  first
+#define S  second
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef std::pair<int, int> pi;
+typedef std::vector<int> vi;
+typedef std::vector<pi> vii;
+
+const int MOD = (int) 1e9 + 7;
+const int INF = (int) 1e9 + 1;
+const int DEG = (int) 1001;
+
+inline ll GCD(ll a, ll b) {
+    ll r;
+    while (b) {
+        r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+inline ll LCM(ll a, ll b) { return a / GCD(a, b) * b; }
+inline bool PRIME(int n) {
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return n > 1;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+#include "bits/macros.h"
 #include "bits/stdc++.h"
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 
     return 0;
 }
